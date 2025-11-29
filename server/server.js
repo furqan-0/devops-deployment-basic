ed9e78134b81614add5c77cb50be06df741c3c0e
Muhammad Furqan <furqan.b2002@gmail.com>
2025-11-30 01:26:16 +0500

    changes happen

---
+++ server/server.js
@@ -0,0 +1,9 @@
+// docker run -p 5000:5000 --name backend --env-file .env mern-backend
+
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('server is running on port 3000');
+});
